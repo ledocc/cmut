9d56549ac265178b139019b08057f007642e954a
David Callu <callu.david@gmail.com>
2015-09-08 10:48:17 +0200

    fix(win32 mp option) : set default to ON

--- cmut_win32_use_mp_option.cmake
+++ cmut_win32_use_mp_option.cmake
@@ -5,9 +5,9 @@
 
 macro( CMUT_WIN32_USE_MP_OPTION )
 
-    if(WIN32 AND MSVC)
+    if(CMAKE_HOST_WIN32 AND MSVC)
         # This option is to enable the /MP switch for Visual Studio 2005 and above compilers
-        option(CMUT_WIN32_USE_MP "Set to ON to build with the /MP option (Visual Studio 2005 and above)." OFF)
+        option(CMUT_WIN32_USE_MP "Set to ON to build with the /MP option (Visual Studio 2005 and above)." ON)
         mark_as_advanced(CMUT_WIN32_USE_MP)
 
         if(CMUT_WIN32_USE_MP)
