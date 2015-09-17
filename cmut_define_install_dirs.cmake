d37ffbb1ef47aafa06e88f208926e3572448cfab
David Callu <callu.david@gmail.com>
2015-09-17 16:33:38 +0200

    fix(install) : fix MacOS support

--- cmut_define_install_dirs.cmake
+++ cmut_define_install_dirs.cmake
@@ -29,17 +29,24 @@ macro( cmut_define_install_dirs )
 
 endmacro()
 
-
 function( cmut_define_install_app_bundle_dirs target)
-    if(CMAKE_HOST_APPLE)
-        set(BUNDLE_ROOT_DIR ${target}.app/Contents)
-        set(CMUT_INSTALL_BUNDLE_BINDIR           ${BUNDLE_ROOT_DIR}/MacOS         PARENT_SCOPE)
-        set(CMUT_INSTALL_BINDIR                  ${CMUT_INSTALL_BUNDLE_BINDIR}    PARENT_SCOPE)
-        set(CMUT_INSTALL_BUNDLE_LIBDIR           ${BUNDLE_ROOT_DIR}/Frameworks    PARENT_SCOPE)
-        set(CMUT_INSTALL_LIBDIR                  ${CMUT_INSTALL_BUNDLE_LIBDIR}    PARENT_SCOPE)
-        set(CMUT_INSTALL_BUNDLE_RESOURCESDIR     ${BUNDLE_ROOT_DIR}/Resources     PARENT_SCOPE)
-        set(CMUT_INSTALL_BUNDLE_PLUGINSDIR       ${BUNDLE_ROOT_DIR}/PlugIns       PARENT_SCOPE)
-        set(CMUT_INSTALL_BUNDLE_SHAREDSUPPORTDIR ${BUNDLE_ROOT_DIR}/SharedSupport PARENT_SCOPE)
-        set(CMUT_INSTALL_BUNDLE_INFO_PLISTDIR    ${BUNDLE_ROOT_DIR}               PARENT_SCOPE)
-    endif()
+
+  if(CMAKE_HOST_APPLE)
+      if(NOT CMUT_DEFINE_INSTALL_DIRS_DONE)
+          cmut_define_install_dirs()
+      endif()
+
+      set(BUNDLE_ROOT_DIR ${CMUT_INSTALL_BINDIR}/${target}.app/Contents)
+
+      set(BUNDLE_BINDIR           ${BUNDLE_ROOT_DIR}/MacOS)
+      set(BUNDLE_LIBDIR           ${BUNDLE_ROOT_DIR}/Frameworks)
+
+      set(CMUT_INSTALL_BUNDLE_BINDIR           ${BUNDLE_BINDIR}                 PARENT_SCOPE)
+      set(CMUT_INSTALL_BUNDLE_LIBDIR           ${BUNDLE_LIBDIR}                 PARENT_SCOPE)
+      set(CMUT_INSTALL_BUNDLE_RESOURCESDIR     ${BUNDLE_ROOT_DIR}/Resources     PARENT_SCOPE)
+      set(CMUT_INSTALL_BUNDLE_PLUGINSDIR       ${BUNDLE_ROOT_DIR}/PlugIns       PARENT_SCOPE)
+      set(CMUT_INSTALL_BUNDLE_SHAREDSUPPORTDIR ${BUNDLE_ROOT_DIR}/SharedSupport PARENT_SCOPE)
+      set(CMUT_INSTALL_BUNDLE_INFO_PLISTDIR    ${BUNDLE_ROOT_DIR}               PARENT_SCOPE)
+  endif()
+
 endfunction()
