67ac4c23b2020506982dfe66d116856db5373624
David Callu <callu.david@gmail.com>
2015-09-23 15:09:55 +0200

    fix(enable_warning) : don't remove all -W*
    with gcc option compatible compilor, don't remove all -w*,
    just remove duplicate entry

--- cmut_enable_warning.cmake
+++ cmut_enable_warning.cmake
@@ -5,7 +5,7 @@
 ###############################################################################
 
 
-# define common variable for any gcc compatible flags, like clang 
+# define common variable for any gcc compatible flags, like clang
 set(__CMUT_WARNING_FLAGS_GNU_COMPAT_COMPILER "-W -Wall")
 set(__CMUT_WARNING_PATTERN_GNU_COMPAT_COMPILER "-W[a-zA-Z0-9]*")
 
@@ -30,13 +30,14 @@ endif()
 set(CMUT_CXX_FLAGS_WARNING ${__CMUT_WARNING_FLAGS} CACHE STRING "Flags use by the compiler to enable warning.")
 message(STATUS "Use C++ flags for warning : ${CMUT_CXX_FLAGS_WARNING}.")
 
-if(CMAKE_CXX_FLAGS)
+if(CMAKE_CXX_FLAGS AND MSVC)
     # remove warning flag provide by cmake
     string(REGEX REPLACE ${__CMUT_WARNING_FLAGS_PATTERN} "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
 ENDIF()
 
 # add CMUT_CXX_FLAGS_WARNING to CMAKE_CXX_FLAGS
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${__CMUT_WARNING_FLAGS}")
+list(REMOVE_DUPLICATES CMAKE_CXX_FLAGS)
 
 # debug only
 #message("CMAKE_CXX_FLAGS = ${CMAKE_CXX_FLAGS}")
