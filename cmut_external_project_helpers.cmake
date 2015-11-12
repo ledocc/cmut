731b55d8e61cb4f6f5ce7b9247ab02ef10dc9c84
David Callu <callu.david@gmail.com>
2015-11-12 07:52:42 +0100

    feat(external_project): "cmut"isation of functionvariable name

--- cmut_external_project_helpers.cmake
+++ cmut_external_project_helpers.cmake
@@ -1,20 +1,20 @@
 
 
-macro(set_version LibName version)
+macro(cmut_set_version LibName version)
 
-    set(${LibName}_VERSION ${version} CACHE STRING "${LibName} version to build")
+    set(CMUT_BUILD_${LibName}_VERSION ${version} CACHE STRING "${LibName} version to build")
 
 endmacro()
 
-macro(set_final_prefix LibName)
+macro(cmut_set_final_prefix LibName)
 
-    set(${LibName}_INSTALL_FINAL_PREFIX CACHE PATH "directory add to CMAKE_INSTALL_PREFIX to define final installation prefix")
-    mark_as_advanced(${LibName}_INSTALL_FINAL_PREFIX)
+    set(CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX CACHE PATH "directory add to CMAKE_INSTALL_PREFIX to define final installation prefix")
+    mark_as_advanced(CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX)
 
-    set(${LibName}_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
+    set(CMUT_BUILD_${LibName}_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
 
-    if (${LibName}_INSTALL_FINAL_PREFIX)
-        set(${LibName}_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}/${${LibName}_INSTALL_FINAL_PREFIX})
+    if (CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX)
+        set(CMUT_BUILD_${LibName}_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}/${CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX})
     endif()
 
 endmacro()
