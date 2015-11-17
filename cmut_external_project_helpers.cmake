9b76c2e637dfac1da94ee08ab66f17b11ac3704a
David Callu <callu.david@gmail.com>
2015-11-17 06:34:24 +0100

    feat(external project helpers): rename variables from CMUT_BUILD_* to CMUT_EP_*

--- cmut_external_project_helpers.cmake
+++ cmut_external_project_helpers.cmake
@@ -1,28 +1,34 @@
 
 
-macro(cmut_set_version LibName version)
+macro(cmut_define_EP_build_option LibName)
 
-    set(CMUT_BUILD_${LibName}_VERSION ${version} CACHE STRING "${LibName} version to build")
+    option(CMUT_EP_BUILD_${LibName} OFF "Enable build of ${LibName}")
+    
+endmacro()
+
+macro(cmut_define_EP_version LibName version)
+
+    set(CMUT_EP_${LibName}_VERSION ${version} CACHE STRING "${LibName} version to build")
 
 endmacro()
 
-macro(cmut_set_default_final_prefix LibName Value)
+macro(cmut_set_EP_default_final_install_prefix LibName Value)
 
-    if(NOT CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX)
-        set(CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX ${Value} CACHE PATH "directory add to CMAKE_INSTALL_PREFIX to define final installation prefix" FORCE)
+    if(CMUT_EP_BUILD_${LibName} AND (NOT CMUT_EP_${LibName}_FINAL_INSTALL_PREFIX))
+        set(CMUT_EP_${LibName}_FINAL_INSTALL_PREFIX ${Value} CACHE PATH "directory add to CMAKE_INSTALL_PREFIX to define final installation prefix" FORCE)
     endif()
     
 endmacro()
 
-macro(cmut_set_final_prefix LibName)
+macro(cmut_define_EP_final_install_prefix LibName)
 
-    set(CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX CACHE PATH "directory add to CMAKE_INSTALL_PREFIX to define final installation prefix")
-    mark_as_advanced(CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX)
+    set(CMUT_EP_${LibName}_FINAL_INSTALL_PREFIX CACHE PATH "directory add to CMAKE_INSTALL_PREFIX to define final installation prefix")
+    mark_as_advanced(CMUT_EP_${LibName}_FINAL_INSTALL_PREFIX)
 
-    set(CMUT_BUILD_${LibName}_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
+    set(CMUT_EP_${LibName}_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
 
-    if (CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX)
-        set(CMUT_BUILD_${LibName}_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}/${CMUT_BUILD_${LibName}_INSTALL_FINAL_PREFIX})
+    if (CMUT_EP_${LibName}_FINAL_INSTALL_PREFIX)
+        set(CMUT_EP_${LibName}_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}/${CMUT_EP_${LibName}_FINAL_INSTALL_PREFIX})
     endif()
 
 endmacro()
