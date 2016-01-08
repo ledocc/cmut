ed5e108f38e012189ce1a84d56b649d12cd42dbd
David Callu <callu.david@gmail.com>
2016-01-09 00:18:55 +0100

    feat(add_boost_test): add boost include and lib

--- cmut_boost_test.cmake
+++ cmut_boost_test.cmake
@@ -9,10 +9,20 @@ function(cmut_add_boost_test test_src_file dependency_lib_list)
     get_filename_component(_test_exec_name ${test_src_file} NAME_WE)
 
     add_executable(${_test_exec_name} ${test_src_file})
-    
-    target_link_libraries(${_test_exec_name}
-        ${${dependency_lib_list}} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
-    
+
+    target_include_directories(
+        ${_test_exec_name} SYSTEM
+        PUBLIC ${BOOST_INCLUDE_DIR}
+    )
+
+    target_link_libraries(
+        ${_test_exec_name}
+        ${${dependency_lib_list}}
+        ${Boost_CHRONO_LIBRARIES}
+        ${Boost_TIMER_LIBRARIES}
+        ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+    )
+
     file(READ "${test_src_file}" _contents)
     string(REGEX MATCHALL "BOOST_[A-Z]+_TEST_CASE\\([*, A-Za-z_0-9]+\\)"
         _test_instances ${_contents})
