a27bddce6b4c5dab3365945de679c96540ed7d30
David Callu <callu.david@gmail.com>
2016-09-08 15:41:51 +0200

    feat(message): add color

--- cmut_message.cmake
+++ cmut_message.cmake
@@ -3,11 +3,32 @@ cmut__utils__define_header_guard()
 
 
 
+include(${CMAKE_CURRENT_LIST_DIR}//cmut_color_message.cmake)
+
+
+
 set(ENV{__cmut_message_count} 0)
 function(cmut_message status severity message)
 
+    if(${ARGC} GREATER 3)
+        set(color_message ${ARGV3})
+        set(reset_color_message ${cmut_color_message_ColorReset})
+    else()
+        set(color_message "")
+        set(reset_color_message "")
+    endif()
+
+    
     string(TIMESTAMP timestamp)
-    message(${status} "[cmut] [${timestamp}] $ENV{__cmut_message_count}  <${severity}> : ${message}")
+
+    set(__message
+"${cmut_color_message_BoldBlue} [cmut] ${cmut_color_message_ColorReset} \
+[${timestamp}] $ENV{__cmut_message_count}  ${cmut_color_message_BoldBlue}<${severity}>${cmut_color_message_ColorReset} : \
+${color_message}${message}${reset_color_message}"
+        )
+
+    
+    message(${status} "${__message}")
 
     math(EXPR result "$ENV{__cmut_message_count} + 1")
     set(ENV{__cmut_message_count} ${result})
@@ -16,22 +37,22 @@ endfunction()
 
 function(cmut_debug message)
     if(CMUT_DEBUG)
-        cmut_message(STATUS "debug" "${message}")
+        cmut_message(STATUS "debug" "${message}" )
     endif()
 endfunction()
 
 function(cmut_info message)
-    cmut_message(STATUS "info" "${message}")
+    cmut_message(STATUS "info" "${message}" ${cmut_color_message_Green})
 endfunction()
 
 function(cmut_warn message)
-    cmut_message(WARNING "error" "${message}")
+    cmut_message(WARNING "error" "${message}" ${cmut_color_message_Red})
 endfunction()
 
 function(cmut_error message)
-    cmut_message(SEND_ERROR "error" "${message}")
+    cmut_message(SEND_ERROR "error" "${message}" ${cmut_color_message_BoldRed})
 endfunction()
 
 function(cmut_fatal message)
-    cmut_message(FATAL_ERROR "fatal" "${message}")
+    cmut_message(FATAL_ERROR "fatal" "${message}" ${cmut_color_message_BoldRed})
 endfunction()
