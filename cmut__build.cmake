1e5ecfbba8c1b499da64aed89afc9e519fc3ff1d
David Callu <callu.david@gmail.com>
2016-10-19 10:45:05 +0200

    feat(build): add coverage target

--- cmut__build.cmake
+++ cmut__build.cmake
@@ -1,4 +1,5 @@
 
+include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__coverage.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__enable_cxx_standard.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__enable_runpath_link.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__enable_warning.cmake)
