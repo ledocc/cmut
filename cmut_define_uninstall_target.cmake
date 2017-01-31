ad02132f61d782ed05075c8aa25cee8da0a0b1fc
David Callu <callu.david@gmail.com>
2017-01-31 15:10:50 +0100

    fix(debug): remove useless debug log

--- cmut_define_uninstall_target.cmake
+++ cmut_define_uninstall_target.cmake
@@ -1,9 +1,3 @@
-
-
-        message("CMAKE_CURRENT_LIST_FILE = ${CMAKE_CURRENT_LIST_FILE}")
-        message("CMAKE_CURRENT_LIST_DIR = ${CMAKE_CURRENT_LIST_DIR}")
-
-
-
 include(${CMAKE_CURRENT_LIST_DIR}/function/cmut_define_uninstall_target.cmake)
+
 cmut_define_uninstall_target()
