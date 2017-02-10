cb72285ddde8a6d051740effc94bb9b4ad13056d
Bilal Afkir <bafkir@unowhy.com>
2017-02-10 20:34:28 +0100

    feat(build): add msvc runtime link type

--- build/cmut__build__msvc.cmake
+++ build/cmut__build__msvc.cmake
@@ -1,3 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/msvc/cmut__build__msvc__nominmax.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/msvc/cmut__build__msvc__option_use_mp.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/msvc/cmut__build__msvc__secure_no_warning.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/msvc/cmut__build__msvc__set_runtime_link_type.cmake)
---
+++ build/msvc/cmut__build__msvc__set_runtime_link_type.cmake
@@ -0,0 +1,36 @@
+include(${CMAKE_CURRENT_LIST_DIR}/../../utils/cmut__utils__header_guard.cmake)
+cmut__utils__define_header_guard()
+
+
+
+function(cmut__build__msvc__set_runtime_link_type link_type)
+
+    if(NOT MSVC)
+        return()
+    endif()
+
+    if("${link_type}" STREQUAL "STATIC")
+        set(runtime_link_type_option /MT)
+    elseif(${link_type} STREQUAL "DYNAMIC")
+        set(runtime_link_type_option /MD)
+    else()
+        cmut_error("cmut__build__msvc__set_runtime_link_type : \"${link_type}\" not a valid runtime link type")
+    endif()
+    
+    if ("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+        set(runtime_link_type_option ${runtime_link_type_option}d)
+    endif()
+    
+#    target_compile_options(${target} PUBLIC "${runtime_link_type_option}")
+    
+    
+    if(DEFINED CMAKE_BUILD_TYPE)
+        string(TOUPPER  ${CMAKE_BUILD_TYPE} UPPER_CASE)
+        set(VARIABLE_SUFFIX _${UPPER_CASE})
+    endif()
+    
+    set(VARIABLE_NAME CMAKE_CXX_FLAGS${VARIABLE_SUFFIX})
+    
+    string(REGEX REPLACE "/M[DT]d?" "${runtime_link_type_option}" ${VARIABLE_NAME} "${${VARIABLE_NAME}}")
+    set(${VARIABLE_NAME} "${${VARIABLE_NAME}}" PARENT_SCOPE)
+endfunction()
