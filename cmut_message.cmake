06c830c3e62b40685dad1946027c0720f293fedb
David Callu <callu.david@gmail.com>
2017-03-08 14:33:37 +0100

    fix(message): remove blinking color of fatal message

--- cmut_message.cmake
+++ cmut_message.cmake
@@ -9,25 +9,29 @@ include(${CMAKE_CURRENT_LIST_DIR}//cmut_color_message.cmake)
 
 set(cmut_message_cmut_color ${cmut_color_message_Bold}${cmut_color_message_Blue})
 set(cmut_message_info_color ${cmut_color_message_Green})
+set(cmut_message_info_message_color ${cmut_message_info_color})
 set(cmut_message_warn_color ${cmut_color_message_Red})
+set(cmut_message_warn_message_color ${cmut_message_warn_color})
 set(cmut_message_error_color ${cmut_color_message_Bold}${cmut_message_warn_color})
+set(cmut_message_error_message_color ${cmut_message_error_color})
 set(cmut_message_fatal_color ${cmut_color_message_Blink}${cmut_message_error_color})
+set(cmut_message_fatal_message_color ${cmut_message_error_color})
 
 
 
 set(ENV{__cmut_message_count} 0)
 function(cmut_message status severity message)
-    
+
     string(TIMESTAMP timestamp)
-        
+
     set(__message
 "\
 ${cmut_message_cmut_color}[cmut] ${cmut_color_message_Reset} \
 [${timestamp}] $ENV{__cmut_message_count} \
 ${cmut_message_${severity}_color}<${severity}>${cmut_color_message_Reset} : \
-${cmut_message_${severity}_color}${message}${cmut_color_message_Reset}"
+${cmut_message_${severity}_message_color}${message}${cmut_color_message_Reset}"
         )
-    
+
     message(${status} "${__message}")
 
     math(EXPR result "$ENV{__cmut_message_count} + 1")
