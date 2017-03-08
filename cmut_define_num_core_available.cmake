a263078080d4f59ceccaddc7ebd61803c4c469b5
David Callu <callu.david@gmail.com>
2017-03-08 14:31:44 +0100

    feat(num_core): use newer function to get num logical core

--- cmut_define_num_core_available.cmake
+++ cmut_define_num_core_available.cmake
@@ -1,6 +1,6 @@
 # define CMUT_NUM_CORE_AVAILABLE with number of available core on this machine
-# 
-# CMUT_NUM_CORE_AVAILABLE is guarantee to be at least equal to 1 
+#
+# CMUT_NUM_CORE_AVAILABLE is guarantee to be at least equal to 1
 
 
 if(DEFINED ${CMAKE_CURRENT_LIST_FILE}_include)
@@ -9,12 +9,12 @@ endif()
 set(${CMAKE_CURRENT_LIST_FILE}_include "1")
 
 
-include(ProcessorCount)
-ProcessorCount(__cmut_num_core_available)
+cmake_host_system_information(
+    RESULT __cmut_num_core_available
+    QUERY  NUMBER_OF_LOGICAL_CORES)
 
 if(__cmut_num_core_available LESS 1)
     set(__cmut_num_core_available 1)
 endif()
 
 set(CMUT_NUM_CORE_AVAILABLE ${__cmut_num_core_available} CACHE STRING "number of parallel job used to build")
-
