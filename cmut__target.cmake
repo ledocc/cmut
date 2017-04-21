240bba5e31932e964dfdbc75adeb84af761d9932
David Callu <callu.david@gmail.com>
2017-04-21 20:46:17 +0200

    feat(target/unix): add function to copy/extract/both debug info from a target

--- cmut__target.cmake
+++ cmut__target.cmake
@@ -1,4 +1,5 @@
 
-include(${CMAKE_CURRENT_LIST_DIR}/target/cmut__target__generate_export_header.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/target/cmut__target__set_library_version.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/target/cmut__target__win32.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/target/cmut__target__generate_export_header.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/target/cmut__target__set_library_version.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/target/cmut__target__unix.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/target/cmut__target__win32.cmake")
---
+++ target/cmut__target__unix.cmake
@@ -0,0 +1,1 @@
+include("${CMAKE_CURRENT_LIST_DIR}/unix/cmut__target__unix__debug_info.cmake")
