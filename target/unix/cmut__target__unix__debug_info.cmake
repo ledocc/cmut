c9ba794ccb49c443e8c70f8d4e827e320c0d470e
David Callu <callu.david@gmail.com>
2017-04-27 15:28:39 +0200

    feat(target): allow user to disable target dependency in *_debug_info function

--- target/unix/cmut__target__unix__debug_info.cmake
+++ target/unix/cmut__target__unix__debug_info.cmake
@@ -1,4 +1,4 @@
-
+include("${CMAKE_CURRENT_LIST_DIR}/../../utils/cmut__utils__parse_arguments.cmake")
 
 
 
@@ -26,30 +26,70 @@ endfunction()
 
 
 function(cmut__target__unix__copy_debug_info target output_file)
+
+    cmut__utils__parse_arguments(cmut__target__unix__copy_debug_info
+        "ARG"
+        "NO_DEPENDS_TARGET"
+        ""
+        ""
+        ${ARGN}
+        )
+
+    if(NOT ARG_NO_DEPENDS_TARGET)
+        set(depends_arg DEPENDS ${target})
+    endif()
+
     find_program(OBJCOPY_COMMAND objcopy)
 
     __cmut__target__unix__get_copy_debug_info_command(${target} ${output_file} copy_debug_info_command)
 
     add_custom_target(copy_${target}_debug_info ${copy_debug_info_command}
         COMMENT "copy debug info from \"${target}\" to ${output_file}"
-        DEPENDS ${target}
+        ${depends_arg}
     )
 
 endfunction()
 
 function(cmut__target__unix__strip_debug_info target)
+
+    cmut__utils__parse_arguments(cmut__target__unix__copy_debug_info
+        "ARG"
+        "NO_DEPENDS_TARGET"
+        ""
+        ""
+        ${ARGN}
+        )
+
+    if(NOT ARG_NO_DEPENDS_TARGET)
+        set(depends_arg DEPENDS ${target})
+    endif()
+
+
     find_program(OBJCOPY_COMMAND objcopy)
 
     __cmut__target__unix__get_strip_debug_info_command(${target} strip_debug_info_command)
 
     add_custom_target(strip_${target}_debug_info ${strip_debug_info_command}
         COMMENT "strip debug info from \"${target}\""
-        DEPENDS ${target}
+        ${depends_arg}
     )
 
 endfunction()
 
 function(cmut__target__unix__copy_and_strip_debug_info target output_file)
+
+    cmut__utils__parse_arguments(cmut__target__unix__copy_debug_info
+        "ARG"
+        "NO_DEPENDS_TARGET"
+        ""
+        ""
+        ${ARGN}
+        )
+
+    if(NOT ARG_NO_DEPENDS_TARGET)
+        set(depends_arg DEPENDS ${target})
+    endif()
+
 
     find_program(OBJCOPY_COMMAND objcopy)
 
@@ -60,7 +100,7 @@ function(cmut__target__unix__copy_and_strip_debug_info target output_file)
         COMMAND ${copy_debug_info_command}
         COMMAND ${strip_debug_info_command}
         COMMENT "copy debug info from \"${target}\" to ${output_file}, then strip debug info from \"${target}\""
-        DEPENDS ${target}
+        ${depends_arg}
     )
 
 endfunction()
