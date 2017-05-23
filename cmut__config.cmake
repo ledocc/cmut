5f4c400361bb1137fe303a78723d0e69b60be6fa
David Callu <callu.david@gmail.com>
2017-05-23 17:56:30 +0200

    feat(install): add function helper to convert CMAKE_INSTALL_PREFIX in absolute file if is not

--- cmut__config.cmake
+++ cmut__config.cmake
@@ -1,3 +1,4 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__log_project.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_developer_mode.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__resolve_install_prefix.cmake")
