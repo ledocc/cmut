0a82365aa90d17c691cf53daaa8b92f05478029a
David Callu <callu.david@gmail.com>
2017-07-04 21:49:35 +0200

    feat(utils): add set_version, get_build_time and improve parse_version

--- target/cmut__target__set_library_version.cmake
+++ target/cmut__target__set_library_version.cmake
@@ -9,24 +9,24 @@ include(${CMAKE_CURRENT_LIST_DIR}/../utils/cmut__utils__parse_version.cmake)
 
 function(cmut__target__set_library_version target version)
 
-    cmut__utils__parse_version(${version} major minor patch)
 
-    
+    cmut__utils__parse_version(__cmut__lib ${version})
+
+
     get_target_property(library_type ${target} TYPE)
     if(NOT ${library_type} STREQUAL "INTERFACE_LIBRARY")
-        
+
         set_target_properties(${target}
         PROPERTIES
             VERSION                           ${version}
-            SOVERSION                         ${major}
+            SOVERSION                         ${__cmut__lib_VERSION_MAJOR}
         )
 
     endif()
 
-    
     set_target_properties(${target}
         PROPERTIES
-            INTERFACE_${target}_MAJOR_VERSION ${major}
+            INTERFACE_${target}_MAJOR_VERSION ${__cmut__lib_VERSION_MAJOR}
             COMPATIBLE_INTERFACE_STRING       ${target}_MAJOR_VERSION
         )
 
--- utils/cmut__utils__parse_version.cmake
+++ utils/cmut__utils__parse_version.cmake
@@ -6,32 +6,36 @@ cmut__utils__define_header_guard()
 include(${CMAKE_CURRENT_LIST_DIR}/../cmut_message.cmake)
 
 
-function(cmut__utils__parse_version version __major __minor __patch)
-
-    set(version_regex_component_content "[0-9A-Za-z-]")
-    set(version_regex_component "${version_regex_component_content}+")
-    set(version_regex_separator "\\.")
-    
-    set(TWO_PART_VERSION_REGEX "${version_regex_component}${version_regex_separator}${version_regex_component}")
-    set(THREE_PART_VERSION_REGEX "${TWO_PART_VERSION_REGEX}${version_regex_separator}${version_regex_component}")
-
-    if(${version} MATCHES ${THREE_PART_VERSION_REGEX})
-        string(REGEX REPLACE "^(${version_regex_component})${version_regex_separator}${version_regex_component}${version_regex_separator}${version_regex_component}" "\\1" __major_version "${version}")
-        string(REGEX REPLACE "^${version_regex_component}${version_regex_separator}(${version_regex_component})${version_regex_separator}${version_regex_component}" "\\1" __minor_version "${version}")
-        string(REGEX REPLACE "^${version_regex_component}${version_regex_separator}${version_regex_component}${version_regex_separator}(${version_regex_component})" "\\1" __patch_version "${version}")
-    elseif(${version} MATCHES ${TWO_PART_VERSION_REGEX})
-        string(REGEX REPLACE "^(${version_regex_component})${version_regex_separator}${version_regex_component}" "\\1" __major_version "${version}")
-        string(REGEX REPLACE "^${version_regex_component}${version_regex_separator}(${version_regex_component})" "\\1" __minor_version "${version}")
-        set(${__patch_version} 0)
+macro(__cmut__utils__set_or_default_in_parent_scope name value default)
+    if("${value}" STREQUAL "")
+        set(${name} ${default} PARENT_SCOPE)
     else()
-        cmut_error("cmut__utils__parse_version: fail to parse version \"${version}\"")
-        return()
+        set(${name} ${value} PARENT_SCOPE)
+    endif()
+endmacro()
+
+macro(__cmut__utils__parse_version name_ version_ functionName_)
+
+    set(version_regex "([0-9]+)(\\.([0-9]+))?(\\.([0-9]+))?(\\.([0-9]+))?(-rc([0-9]+))?")
+
+    if (NOT ${version_} MATCHES ${version_regex})
+        cmut_error("${functionName_} : invalid version format : ${version_}\n")
     endif()
 
 
-    set(${__major} ${__major_version} PARENT_SCOPE)
-    set(${__minor} ${__minor_version} PARENT_SCOPE)
-    set(${__patch} ${__patch_version} PARENT_SCOPE)
+    __cmut__utils__set_or_default_in_parent_scope(${name_}_VERSION_MAJOR     "${CMAKE_MATCH_1}" 0 PARENT_SCOPE)
+    __cmut__utils__set_or_default_in_parent_scope(${name_}_VERSION_MINOR     "${CMAKE_MATCH_3}" 0 PARENT_SCOPE)
+    __cmut__utils__set_or_default_in_parent_scope(${name_}_VERSION_PATCH     "${CMAKE_MATCH_5}" 0 PARENT_SCOPE)
+    __cmut__utils__set_or_default_in_parent_scope(${name_}_VERSION_TWEAK     "${CMAKE_MATCH_7}" 0 PARENT_SCOPE)
+    __cmut__utils__set_or_default_in_parent_scope(${name_}_RELEASE_CANDIDATE "${CMAKE_MATCH_9}" 0 PARENT_SCOPE)
 
-    
+endmacro()
+
+
+
+
+function(cmut__utils__parse_version name_ version_)
+
+    __cmut__utils__parse_version(${name_} ${version_} cmut__utils__parse_version)
+
 endfunction()
