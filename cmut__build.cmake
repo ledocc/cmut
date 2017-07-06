4500e519a865fb036807b76022410dc7951ca95f
David Callu <callu.david@gmail.com>
2017-07-06 14:00:14 +0200

    feat(build): add cmut__build__enable_color_with_ninja

--- cmut__build.cmake
+++ cmut__build.cmake
@@ -1,5 +1,6 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__coverage.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__enable_color_with_ninja.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__enable_cxx_standard.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__enable_runpath_link.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/build/cmut__build__enable_warning.cmake)
