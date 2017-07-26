5e97e5c11fec88e557e4535da5f3287be8384d56
David Callu <callu.david@gmail.com>
2017-07-26 23:57:46 +0200

    feat(config): add option for include-what-you-use

--- cmut__config.cmake
+++ cmut__config.cmake
@@ -2,4 +2,5 @@
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__log_project.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_clang_tidy.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_developer_mode.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_include_what_you_use.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__resolve_install_prefix.cmake")
