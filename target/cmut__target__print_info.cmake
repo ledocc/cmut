08ffd8b867a25f0f2783a9931212c008e9921fa2
David Callu <callu.david@gmail.com>
2017-08-11 14:34:13 +0200

    fix(target): add many fix to cmut__target__print_info that never work

--- target/cmut__target__print_info.cmake
+++ target/cmut__target__print_info.cmake
@@ -14,36 +14,46 @@ function(cmut__target__print_info target_)
         ""
         ""
         "COMPONENTS"
+        ${ARGN}
         )
 
     if(ARG_COMPONENTS)
-        foreach(component IN_LISTS ARG_COMPONENTS)
+        foreach(component IN LISTS ARG_COMPONENTS)
             list(APPEND targets ${target_}::${component})
         endforeach()
     else()
         set(targets ${target_})
     endif()
 
-
-    foreach(target IN LISTS ${targets})
-
-        if(NOT TARGET ${target})
-            string(TOLOWER ${target} target)
-        endif()
+    foreach(target IN LISTS targets)
 
         if(NOT TARGET ${target})
-            cmut_error("\"${target}\" is not a target.")
-            continue()
+            string(TOLOWER ${target} target_lower)
+
+            if(NOT TARGET ${target_lower})
+                cmut_error("\"${target}\" or \"${target_lower}\" is not a target.")
+                continue()
+            else()
+                set(target ${target_lower})
+            endif()
         endif()
+
+
+        set(targetPropertiesToPrint)
+        get_target_property(targetType ${target} TYPE)
+        if (NOT targetType STREQUAL "INTERFACE_LIBRARY")
+            list(APPEND targetPropertiesToPrint LOCATION)
+        endif()
+        list(APPEND targetPropertiesToPrint
+            INTERFACE_INCLUDE_DIRECTORIES
+            INTERFACE_LINK_LIBRARIES
+        )
 
         cmake_print_properties(
             TARGETS
                 ${target}
-            PROPERTIES
-                LOCATION
-                INTERFACE_INCLUDE_DIRECTORIES
-                INTERFACE_LINK_LIBRARIES
-                )
+            PROPERTIES ${targetPropertiesToPrint}
+        )
     endforeach()
 
 endfunction()
