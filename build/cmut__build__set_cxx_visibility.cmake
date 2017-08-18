f0e0f8ef0651bc92a019733109d0772cfa68aec4
David Callu <callu.david@gmail.com>
2017-08-18 16:47:13 +0200

    feat(build): make visibility same on unix and windows

--- build/cmut__build__set_cxx_visibility.cmake
+++ build/cmut__build__set_cxx_visibility.cmake
@@ -11,16 +11,19 @@ function(cmut__build__set_cxx_visibility value)
 
     if(${value} STREQUAL "default")
 
+        set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON PARENT_SCOPE)
         set(CMAKE_CXX_VISIBILITY_PRESET default PARENT_SCOPE)
         set(CMAKE_VISIBILITY_INLINES_HIDDEN OFF PARENT_SCOPE)
 
     elseif(${value} STREQUAL "inlines_hidden")
 
+        set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON PARENT_SCOPE)
         set(CMAKE_CXX_VISIBILITY_PRESET default PARENT_SCOPE)
         set(CMAKE_VISIBILITY_INLINES_HIDDEN ON PARENT_SCOPE)
 
     elseif(${value} STREQUAL "hidden")
 
+        set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS OFF PARENT_SCOPE)
         set(CMAKE_CXX_VISIBILITY_PRESET hidden PARENT_SCOPE)
         set(CMAKE_VISIBILITY_INLINES_HIDDEN ON PARENT_SCOPE)
 
@@ -28,6 +31,7 @@ function(cmut__build__set_cxx_visibility value)
 
         cmut_error("cmut__build__set_cxx_visibility : visibility \"${value}\" not handle")
         return()
+
     endif()
 
 
