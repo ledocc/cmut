a756a725dc812486247e2d3c7b625c32e42183fc
David Callu <callu.david@gmail.com>
2017-08-18 08:56:27 +0200

    feat(config): add option to set BUILD_SHARED_LIBS

--- cmut__config.cmake
+++ cmut__config.cmake
@@ -1,5 +1,6 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__log_project.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_build_shared_libs.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_clang_tidy.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_developer_mode.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_include_what_you_use.cmake")
---
+++ config/cmut__config__option_build_shared_libs.cmake
@@ -0,0 +1,21 @@
+
+
+include(${CMAKE_CURRENT_LIST_DIR}/../utils/cmut__utils__header_guard.cmake)
+cmut__utils__define_header_guard()
+
+
+
+# This option control the link type.
+# When ON, all library will be build in shared mode,
+# when OFF, all library will be build in static mode,
+function(cmut__config__option_build_shared_libs defaultValue)
+
+    option(BUILD_SHARED_LIBS "Enable this option to create shared libraries" ${defaultValue})
+    if(BUILD_SHARED_LIBS)
+        set(CMUT__CONFIG__LINK_TYPE "shared")
+    else()
+        set(CMUT__CONFIG__LINK_TYPE "static")
+    endif()
+    cmut_info("library link type : ${CMUT__CONFIG__LINK_TYPE}")
+
+endfunction()
