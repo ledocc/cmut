0cc358ba56c2b2348100462d79decb1a31460bbf
David Callu <callu.david@gmail.com>
2017-10-16 12:00:25 +0200

    feat(cmut): add cmut_version.cmake

--- cmut.cmake
+++ cmut.cmake
@@ -1,10 +1,11 @@
 
+
 include("${CMAKE_CURRENT_LIST_DIR}/cmut_message.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/cmut_version.cmake")
 
 include("${CMAKE_CURRENT_LIST_DIR}/cmut__build.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmut__config.cmake")
-#include("${CMAKE_CURRENT_LIST_DIR}/cmut__find.cmake")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/find")
+include("${CMAKE_CURRENT_LIST_DIR}/cmut__find.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmut__install.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmut__utils.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmut__system.cmake")
