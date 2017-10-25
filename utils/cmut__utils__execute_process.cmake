f0f6a494adb2e9a839e0b480e354be0bcbc096d6
David Callu <callu.david@gmail.com>
2017-10-25 21:41:40 +0200

    feat(utils): add LOG_FILE option to cmut__utils__execute_process

--- utils/cmut__utils__execute_process.cmake
+++ utils/cmut__utils__execute_process.cmake
@@ -10,7 +10,7 @@ function(cmut__utils__execute_process)
         cmut__utils__execute_process
         ARG
         "FATAL"
-        "WORKING_DIRECTORY"
+        "WORKING_DIRECTORY;LOG_FILE"
         "COMMAND"
         ${ARGN}
         )
@@ -23,17 +23,37 @@ function(cmut__utils__execute_process)
         list(APPEND execute_process_args WORKING_DIRECTORY ${ARG_WORKING_DIRECTORY})
     endif()
 
+    if(ARG_LOG_FILE)
+        list(APPEND execute_process_args OUTPUT_FILE ${ARG_LOG_FILE}-out.log)
+        list(APPEND execute_process_args ERROR_FILE ${ARG_LOG_FILE}-err.log)
+    else()
+        list(APPEND execute_process_args OUTPUT_VARIABLE output_var)
+        list(APPEND execute_process_args ERROR_VARIABLE error_var)
+
+    endif()
+
 
 
     execute_process(
         ${execute_process_args}
-        RESULT_VARIABLE result_var
-        OUTPUT_VARIABLE output_var
-        ERROR_VARIABLE error_var
+        RESULT_VARIABLE result
         )
 
-    if(result_var)
-        set(msg "execute_process failed.\ncommand \"${ARG_COMMAND}\" return ${result_var}.\noutput : ${output_var}\nerror  : ${error_var}")
+
+
+    if(result)
+        set(msg "Command failed: ${result}\n")
+        foreach(arg IN LISTS ARG_COMMAND)
+            set(msg "${msg} '${arg}'")
+        endforeach()
+        set(msg "${msg}\n")
+
+        if(NOT ARG_LOG_FILE)
+            set(msg "${msg}output : ${output_var}\n")
+            set(msg "${msg}error  : ${error_var}")
+        else()
+            set(msg "${msg}See also\n  ${ARG_LOG_FILE}-*.log")
+        endif()
 
         if(ARG_FATAL)
             cmut_fatal("${msg}")
