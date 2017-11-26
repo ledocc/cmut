df30ded53d8ae3dcf4ff24f77f3efa0a9393b7a6
David Callu <callu.david@gmail.com>
2017-11-26 21:24:43 +0100

    fix(cmut__system__get_osx_sdk_short_name): add warning if CMAKE_OSX_SYSROOT is empty

--- system/cmut__system__get_osx_sdk_short_name.cmake
+++ system/cmut__system__get_osx_sdk_short_name.cmake
@@ -1,10 +1,15 @@
 
 
 function(cmut__system__get_osx_sdk_short_name result)
+    
+    if("${CMAKE_OSX_SYSROOT}" STREQUAL "")
+        cmut_warn("[cmut] - cmut__system__get_osx_sdk_short_name : CMAKE_OSX_SYSROOT is empty.")
+        set(${result} "" PARENT_SCOPE)
+    endif()				      
 
-    get_filename_component(name ${CMAKE_OSX_SYSROOT} NAME)
-    string(TOLOWER ${name} name)
-    string(REGEX MATCH "[a-z]+[0-9]+\.[0-9]+" name ${name})
+    get_filename_component(name "${CMAKE_OSX_SYSROOT}" NAME)
+    string(TOLOWER "${name}" name)
+    string(REGEX MATCH "[a-z]+[0-9]+\.[0-9]+" name "${name}")
 
     set(${result} "${name}" PARENT_SCOPE)
 
