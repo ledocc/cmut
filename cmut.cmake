d297c17fec11ec844c35b30459e6601b190e9000
David Callu <callu.david@gmail.com>
2017-12-04 15:57:56 +0100

    feat(cmut): print cmut version when include cmut.cmake

--- cmut.cmake
+++ cmut.cmake
@@ -3,6 +3,11 @@
 include("${CMAKE_CURRENT_LIST_DIR}/cmut_message.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmut_version.cmake")
 
+if(NOT CMUT__MAIN_INCLUDE_QUIET)
+    cmut_info("[cmut] - location : \"${CMUT_ROOT}\".")
+    cmut_info("[cmut] - version  : \"${CMUT_VERSION}\".")
+endif()
+
 include("${CMAKE_CURRENT_LIST_DIR}/cmut__build.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmut__config.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmut__find.cmake")
