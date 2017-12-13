35def8c4e828e4f4b2cad11f34a1283a2eb81647
David Callu <callu.david@gmail.com>
2017-12-13 16:11:03 +0100

    feat(system): use SDKROOT as fallback to get osx sdk short name

--- system/cmut__system__get_osx_sdk_short_name.cmake
+++ system/cmut__system__get_osx_sdk_short_name.cmake
@@ -2,15 +2,23 @@
 
 function(cmut__system__get_osx_sdk_short_name result)
     
-    if("${CMAKE_OSX_SYSROOT}" STREQUAL "")
-        cmut_warn("[cmut] - cmut__system__get_osx_sdk_short_name : CMAKE_OSX_SYSROOT is empty.")
+    if(NOT "${CMAKE_OSX_SYSROOT}" STREQUAL "")
+
+        get_filename_component(name "${CMAKE_OSX_SYSROOT}" NAME)
+        string(TOLOWER "${name}" name)
+        string(REGEX MATCH "[a-z]+[0-9]+\.[0-9]+" name "${name}")
+
+        set(${result} "${name}" PARENT_SCOPE)
+
+    elseif(NOT "$ENV{SDKROOT}" STREQUAL "")
+
+        set(${result} "$ENV{SDKROOT}" PARENT_SCOPE)
+        
+    else()
+
+        cmut_warn("[cmut] - cmut__system__get_osx_sdk_short_name : neither cmake variable \"CMAKE_OSX_SYSROOT\" and environment variable \"SDKROOT\" empty.")
         set(${result} "" PARENT_SCOPE)
-    endif()				      
 
-    get_filename_component(name "${CMAKE_OSX_SYSROOT}" NAME)
-    string(TOLOWER "${name}" name)
-    string(REGEX MATCH "[a-z]+[0-9]+\.[0-9]+" name "${name}")
-
-    set(${result} "${name}" PARENT_SCOPE)
+    endif()
 
 endfunction()
