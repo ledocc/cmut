9dcc6c3045d2159573ab0f94e712a0af920d368c
David Callu <callu.david@gmail.com>
2018-01-09 07:57:12 +0100

    fix(build): improve coverage option
    Reason :
      from gcc's man page:
      --coverage
        This option is used to compile and link code instrumented for coverage
        analysis.  The option is a synonym for -fprofile-arcs -ftest-coverage
        (when compiling) and -lgcov (when linking).
    
    
    Action:
      - use only --coverage option in CMAKE_{C,CXX}_FLAGS_COVERAGE
      - use --coverage option in CMAKE_{EXE,SHARED}_LINKER_FLAGS_COVERAGE

--- build/cmut__build__coverage.cmake
+++ build/cmut__build__coverage.cmake
@@ -91,19 +91,19 @@ function(__cmut__build__init_coverage)
     ENDIF() # CHECK VALID COMPILER
 
     SET(CMAKE_CXX_FLAGS_COVERAGE
-        "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
+        "-g -O0 --coverage"
         CACHE STRING "Flags used by the C++ compiler during coverage builds."
         FORCE )
     SET(CMAKE_C_FLAGS_COVERAGE
-        "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
+        "-g -O0 --coverage"
         CACHE STRING "Flags used by the C compiler during coverage builds."
         FORCE )
     SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE
-        ""
+        "--coverage"
         CACHE STRING "Flags used for linking binaries during coverage builds."
         FORCE )
     SET(CMAKE_SHARED_LINKER_FLAGS_COVERAGE
-        ""
+        "--coverage"
         CACHE STRING "Flags used by the shared libraries linker during coverage builds."
         FORCE )
     MARK_AS_ADVANCED(
