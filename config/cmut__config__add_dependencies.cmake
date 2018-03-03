14d47107c4ef22740a87746f963bba7b6dbbee26
David Callu <callu.david@gmail.com>
2018-03-03 17:21:17 +0100

    feat(config): add cmut__config__add_dependencies function to define dependencies of a project

---
+++ config/cmut__config__add_dependencies.cmake
@@ -0,0 +1,31 @@
+
+function(cmut__config__add_dependencies project package)
+
+    macro(export_to_parent_scope var)
+        message("[cmut][config] - add dependencies : ${var} = ${${var}}")
+        set(${var} ${${var}} PARENT_SCOPE)
+    endmacro()
+
+
+    cmake_parse_arguments(
+        ARG_
+        ""
+        "VERSION"
+        "COMPONENTS"
+        ${ARGN}
+        )
+
+    list(APPEND ${project}_DEPENDENCIES ${package})
+    export_to_parent_scope(${project}_DEPENDENCIES)
+
+    if(DEFINED ARG__VERSION)
+        set(${project}_DEPENDENCIES_${package}_VERSION ${ARG__VERSION})
+        export_to_parent_scope(${project}_DEPENDENCIES_${package}_VERSION)
+    endif()
+
+    if(DEFINED ARG__COMPONENTS)
+        list(APPEND ${project}_DEPENDENCIES_${package}_COMPONENTS ${ARG__COMPONENTS})
+        export_to_parent_scope(${project}_DEPENDENCIES_${package}_COMPONENTS)
+    endif()
+
+endfunction()
