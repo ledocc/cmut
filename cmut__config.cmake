aaf8959b02b3f94e68beedfc4de4ada5e9b3e074
David Callu <callu.david@gmail.com>
2018-03-06 22:45:53 +0100

    feat(config): add cmut__config__print_info

--- cmut__config.cmake
+++ cmut__config.cmake
@@ -10,4 +10,5 @@ include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_clang_tidy.cmake"
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_cppcheck.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_developer_mode.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__option_include_what_you_use.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__print_info.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/config/cmut__config__resolve_install_prefix.cmake")
