18eb461df04448d74d9db044b0070e7f2d766989
Fabien Lassagne <flassagne@unowhy.com>
2018-11-09 15:06:14 +0100

    fix(build): export variable in parent scope

--- build/cmut__build__enable_qml_debugger.cmake
+++ build/cmut__build__enable_qml_debugger.cmake
@@ -6,7 +6,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/../cmut_message.cmake)
 function(cmut__build__enable_qml_debugger)
 
     if ( ( CMAKE_BUILD_TYPE STREQUAL "Debug" ) OR ( CMAKE_BUILD_TYPE STREQUAL "Coverage" ) )
-        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DQT_QML_DEBUG ")
+        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DQT_QML_DEBUG" PARENT_SCOPE)
 
     else()
         cmut_error( "[cmut] - qml debugger can't be use in other BUILD_TYPE than Debug or Coverage, you currently BUILD_TYPE is ${CMAKE_BUILD_TYPE}" )
