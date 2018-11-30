7118da8f5ee077d8ddacdc7343c6d1b77ceba55d
David Callu <callu.david@gmail.com>
2018-11-30 11:44:43 +0100

    fix(dependency): handle resursive call of cmut__dependency__find

--- dependency/cmut__dependency__find.cmake
+++ dependency/cmut__dependency__find.cmake
@@ -1,12 +1,33 @@
 
 
-## cmut__dependency__find( project )
-## brief: call cmake's "find_package" function with data regidtered by "cmut__dependency__add" for the project "project"
+## cmut__dependency__find( project_in )
+## brief: call cmake's "find_package" function with data regidtered by "cmut__dependency__add" for the project "project_in"
 ##
 ## cmut__dependency__find have to be a macro to allow <packageName>Config.cmake script to define variable in global scope
-macro( cmut__dependency__find project )
+## This is a big trouble when script find by find_package (Find<packageName>.cmake or <packageName>Config.cmake) use this same macro.
+## This start a recursive call of cmut__dependency__find, and project variable of outer call is replaced by the one in inner call, and so one.
+## When the inner call return, project variable still have the value of inner call, and all go bad.
+## To fix this, when create a stack that save the current project varible before call find_package, and pop it after call of find_variable.
+macro( cmut__dependency__find project_in )
 
-    macro( __cmut__dependency__set_if_defined opt )
+
+    set(project ${project_in})
+
+
+
+    macro( stack__push value )
+        list( APPEND __CMUT__DEPENDENCY__FIND__STACK ${value} )
+    endmacro()
+
+    macro( stack__pop variable )
+        list( LENGTH    __CMUT__DEPENDENCY__FIND__STACK length )
+        math( EXPR index "${length} - 1")
+        list( GET       __CMUT__DEPENDENCY__FIND__STACK ${index} ${variable} )
+        list( REMOVE_AT __CMUT__DEPENDENCY__FIND__STACK ${index} )
+    endmacro()
+
+
+    macro( set_if_defined opt )
         if( DEFINED ${project}_DEPENDENCIES_${package}_${opt} )
             set( ${opt}_OPTION ${ARGN} ${${project}_DEPENDENCIES_${package}_${opt}} )
         else()
@@ -17,10 +38,10 @@ macro( cmut__dependency__find project )
 
     foreach(package IN LISTS ${project}_DEPENDENCIES)
 
-        __cmut__dependency__set_if_defined( VERSION )
-        __cmut__dependency__set_if_defined( REQUIRED )
-        __cmut__dependency__set_if_defined( COMPONENTS COMPONENTS )
-        __cmut__dependency__set_if_defined( NAMES      NAMES )
+        set_if_defined( VERSION )
+        set_if_defined( REQUIRED )
+        set_if_defined( COMPONENTS COMPONENTS )
+        set_if_defined( NAMES      NAMES )
 
         if(DEFINED ${project}_DEPENDENCIES_${package}_FIND_PACKAGE_NAME)
             set(package ${${project}_DEPENDENCIES_${package}_FIND_PACKAGE_NAME})
@@ -28,12 +49,16 @@ macro( cmut__dependency__find project )
 
 
         message(STATUS "Looking for ${package}")
+
+        stack__push( ${project} )
         find_package(${package}
             ${VERSION_OPTION}
             ${REQUIRED_OPTION}
             ${COMPONENTS_OPTION}
             ${NAMES_OPTION}
             )
+        stack__pop( project )
+
 
     endforeach()
 
