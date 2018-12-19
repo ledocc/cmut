83ed978c1fcbd67baea6c1e5852669b7a78318e0
David Callu <callu.david@gmail.com>
2018-12-19 20:29:06 +0100

    feat(test): add cmut__test__append_env_var and cmut__test__prepend_env_var

--- test/cmut__test__set_test_env_var_property.cmake
+++ test/cmut__test__set_test_env_var_property.cmake
@@ -1,4 +1,29 @@
 
+
+function( cmut__test__append_env_var name env_var )
+
+    get_test_property(${name} ENVIRONMENT test_env_var)
+    list(APPEND test_env_var "${env_var}")
+    set_tests_properties(${name} PROPERTIES ENVIRONMENT "${test_env_var}")
+
+endfunction()
+
+function( cmut__test__prepend_env_var name env_var )
+
+    get_test_property(${name} ENVIRONMENT test_env_var)
+    list(INSERT test_env_var 0 "${env_var}")
+    set_tests_properties(${name} PROPERTIES ENVIRONMENT "${test_env_var}")
+
+endfunction()
+
+
+
+
+
+
+
+
+
 function(cmut__test__set_test_env_var_property env_var namespace)
 
     foreach(test IN LISTS ARGN)
