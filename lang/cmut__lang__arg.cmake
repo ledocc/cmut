4e6b70e93458fada067274ab0c4acc84f1aea7ae
David Callu <callu.david@gmail.com>
2019-01-28 10:51:13 +0100

    feat(lang): add cmut__lang__arg__set_if_defined* functions

--- lang/cmut__lang__arg.cmake
+++ lang/cmut__lang__arg.cmake
@@ -1,5 +1,6 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/arg/cmut__lang__arg__parse.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/arg/cmut__lang__arg__set_default.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/arg/cmut__lang__arg__set_if_defined.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/arg/cmut__lang__arg__set_if_option.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/arg/cmut__lang__arg__test_required.cmake")
