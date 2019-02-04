2061a97d57f801183a6c0aac083e8716cb88cb69
David Callu <callu.david@gmail.com>
2019-02-04 11:28:50 +0100

    feat(conan): add cmut__conan__capture_cxx_standard

--- cmut__conan.cmake
+++ cmut__conan.cmake
@@ -1,2 +1,3 @@
 
+include("${CMAKE_CURRENT_LIST_DIR}/conan/cmut__conan__capture_cxx_standard.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/conan/cmut__conan__download_cmake_conan.cmake")
---
+++ conan/cmut__conan__capture_cxx_standard.cmake
@@ -0,0 +1,8 @@
+
+
+macro(cmut__conan__capture_cxx_standard)
+
+    set(CMAKE_CXX_STANDARD ${CONAN_CMAKE_CXX_STANDARD})
+    set(CMAKE_CXX_EXTENSIONS ${CONAN_CMAKE_CXX_EXTENSIONS})
+
+endmacro()
