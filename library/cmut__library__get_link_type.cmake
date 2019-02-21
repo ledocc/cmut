b218af95ff373ac61c3a3d3cab33a187bcffed4d
David Callu <callu.david@gmail.com>
2019-02-21 07:08:24 +0100

    feat(library): add msvc support of function(cmut__library__get_link_type

--- library/cmut__library__get_link_type.cmake
+++ library/cmut__library__get_link_type.cmake
@@ -9,12 +9,14 @@ function(cmut__library__get_link_type result library_path)
         cmut_fatal("[cmut][library][get_link_type] - \"${library_path}\" : no such file.")
     endif()
 
-    if(WIN32)
-        __cmut__library__get_link_type__win32( local_result "${library_path}" )
+    if(MSVC)
+        __cmut__library__get_link_type__msvc( local_result "${library_path}" )
     elseif(APPLE)
         __cmut__library__get_link_type__apple( local_result "${library_path}" )
-    else()
+    elseif(UNIX)
         __cmut__library__get_link_type__unix( local_result "${library_path}" )
+    else()
+        cmut_fatal("[cmut][library][get_link_type] - platform/compilator not handle. Patch me if you can!")
     endif()
 
     cmut__lang__return(local_result)
@@ -80,7 +82,7 @@ function( __cmut__library__get_link_type__apple result library_path )
         RESULT_VARIABLE objdump_result
         OUTPUT_VARIABLE objdump_output
         ERROR_VARIABLE objdump_error
-	OUTPUT_STRIP_TRAILING_WHITESPACE
+        OUTPUT_STRIP_TRAILING_WHITESPACE
         )
 
     if(objdump_result)
@@ -96,5 +98,34 @@ function( __cmut__library__get_link_type__apple result library_path )
         cmut_fatal("[cmut][library][get_link_type] - \"${library_path}\" : library type not handle : ${objdump_output}.")
     endif()
 
+
+endfunction()
+
+
+function( __cmut__library__get_link_type__msvc result library_path )
+
+    find_program( DUMPBIN_COMMAND dumpbin )
+    if(NOT DUMPBIN_COMMAND)
+        cmut_fatal("[cmut][library][get_link_type] - \"dumpbin\" command not found! cmut can not determine library link type.")
+    endif()
+
+    execute_process(
+        COMMAND "${CMUT_ROOT}/library/windows/get_link_type.bat" "${DUMPBIN_COMMAND}" "${library_path}"
+        OUTPUT_VARIABLE dumpbin_output
+        RESULT_VARIABLE dumpbin_result
+        ERROR_VARIABLE dumpbin_error
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+    if(dumpbin_result)
+        cmut_fatal("[cmut][library][get_link_type] - error while determine \"${library_path}\" link type : ${dumpbin_error}.")
+    endif()
+
+
+    if("${dumpbin_output}" EQUAL 0)
+        cmut__lang__return_value(DYNAMIC_LIBRARY)
+    else()
+        cmut__lang__return_value(STATIC_LIBRARY)
+    endif()
 
 endfunction()
