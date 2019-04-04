6e224605ecd32c611570dbe031e56ce978562c03
David Callu <callu.david@gmail.com>
2019-04-04 22:48:24 +0200

    feat(lang): allow multi value in cmut__lang__return_value

--- lang/cmut__lang__return.cmake
+++ lang/cmut__lang__return.cmake
@@ -5,6 +5,6 @@ macro(cmut__lang__return variable)
     set(${result} "${${variable}}" PARENT_SCOPE)
 endmacro()
 
-macro(cmut__lang__return_value value)
-    set(${result} "${value}" PARENT_SCOPE)
+macro(cmut__lang__return_value )
+    set(${result} "${ARGN}" PARENT_SCOPE)
 endmacro()
