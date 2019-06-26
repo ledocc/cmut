2744978cb8136a4e6e6eaa72d568cb5812313959
David Callu <callu.david@gmail.com>
2019-06-26 11:29:32 +0200

    refactor(cmake_conan): rename cmut__cmake_conan__get_shared_options to cmut__cmake_conan__get_shared_option

--- cmake_conan/cmut__cmake_conan__get_shared_options.cmake
+++ cmake_conan/cmut__cmake_conan__get_shared_options.cmake
@@ -1,6 +1,14 @@
 
-
 function( cmut__cmake_conan__get_shared_options result )
+    cmut_deprecated_function( cmut__cmake_conan__get_shared_options cmut__cmake_conan__get_shared_option )
+
+    cmut__cmake_conan__get_shared_option( result_tmp )
+    cmut__lang__return( result_tmp )
+
+endfunction()
+
+
+function( cmut__cmake_conan__get_shared_option result )
 
     if(BUILD_SHARED_LIBS)
         cmut__lang__return_value(OPTIONS "*:shared=True")
