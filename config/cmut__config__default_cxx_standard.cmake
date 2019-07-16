2c378d3dcbd8c18c970e198cc85ceb4ddf656bd2
David Callu <callu.david@gmail.com>
2019-07-16 18:00:41 +0200

    fix(config): disable default_build_type and default_cxx_standard if CONAN_EXPORTED is defined

--- config/cmut__config__default_cxx_standard.cmake
+++ config/cmut__config__default_cxx_standard.cmake
@@ -7,14 +7,20 @@ function(cmut__config__default_cxx_standard default_cxx_standard )
     cmut__lang__function__parse_arguments( ${ARGN} )
 
     if(NOT DEFINED CMAKE_CXX_STANDARD)
-        set( CMAKE_CXX_STANDARD "${default_cxx_standard}" PARENT_SCOPE )
-        set( CMAKE_CXX_EXTENSIONS ${ARG_EXTENSIONS} PARENT_SCOPE )
 
         set( with_extension_msg )
         if(ARG_EXTENSIONS)
             set( with_extension_msg " with extension")
         endif()
-        cmut_info( "Setting cxx standard to '${default_cxx_standard}${with_extension_msg}' as none was specified." )
+        if( CONAN_EXPORTED )
+            cmut_info( "[cmut][config][default_cxx_standard]: Setting cxx standard to '${default_cxx_standard}${with_extension_msg}' skipped as CONAN_EXPORTED is defined." )
+            return()
+        endif()
+
+        cmut_info( "[cmut][config][default_cxx_standard]: Setting cxx standard to '${default_cxx_standard}${with_extension_msg}' as none was specified." )
+        set( CMAKE_CXX_STANDARD "${default_cxx_standard}" PARENT_SCOPE )
+        set( CMAKE_CXX_EXTENSIONS ${ARG_EXTENSIONS} PARENT_SCOPE )
+
     endif()
 
 endfunction()
