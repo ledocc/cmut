96fce9e95162edb70cf39db8cbf32a79a60f98e7
David Callu <callu.david@gmail.com>
2019-08-26 14:38:07 +0200

    fix(cmut): remove old includes

--- cmut__install.cmake
+++ cmut__install.cmake
@@ -8,8 +8,6 @@ include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__define_variables.cmake
 include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__fixup_bundle_dylib.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__install_cmut__dependency_files.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__install_config_and_version.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__install_config_file.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__install_config_directory.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__install_dependencies.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__install_hunter_3rd_party.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/install/cmut__install__install_library.cmake")
