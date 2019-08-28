480cf25d672856f22ed7b32aea77baf947a206b6
David Callu <callu.david@gmail.com>
2019-08-28 13:45:05 +0200

    refactor(install): mv cmut__install__install_target to cmut__install__target

--- install/component.cmake
+++ install/component.cmake
@@ -1,4 +1,4 @@
 
-include("${CMAKE_CURRENT_LIST_DIR}/component/add_dependency.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/component/get_dependencies.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/component/make_targets.cmake")
+include( "${CMAKE_CURRENT_LIST_DIR}/component/add_dependencies.cmake" )
+include( "${CMAKE_CURRENT_LIST_DIR}/component/get_dependencies.cmake" )
+include( "${CMAKE_CURRENT_LIST_DIR}/component/make_targets.cmake" )
