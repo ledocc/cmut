8aaf8f05f0e703c1257608d76d0f455047e4c3d2
David Callu <callu.david@gmail.com>
2019-09-16 16:15:08 +0200

    fix(target): add missing directory separator in generated_header_output_directory

--- target/cmut__target__get_generated_output_directory.cmake
+++ target/cmut__target__get_generated_output_directory.cmake
@@ -1,6 +1,6 @@
 
 function( cmut__target__get_generated_header_output_directory result target )
 
-    cmut__lang__return_value( "${PROJECT_BINARY_DIR}cmut__target__generated/${target}/include" )
+    cmut__lang__return_value( "${PROJECT_BINARY_DIR}/cmut__target__generated/${target}/include" )
 
 endfunction()
