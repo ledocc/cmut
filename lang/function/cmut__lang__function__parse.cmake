973c9b76a37193458eba32a4a31bad738d6198cf
David Callu <callu.david@gmail.com>
2019-10-17 15:21:35 +0200

    refactor(lang): improve debug info of cmut__lang__function

--- lang/function/cmut__lang__function__parse.cmake
+++ lang/function/cmut__lang__function__parse.cmake
@@ -1,3 +1,4 @@
+#set(CMUT__LANG__FUNCTION__DEBUG 1)
 
 function( cmut__lang__function__init_param function_name )
 
@@ -7,12 +8,24 @@ function( cmut__lang__function__init_param function_name )
     cmut__lang__set_in_parent_scope( __cmut__lang__function__arg_prefix ARG)
     cmut__lang__set_in_parent_scope( __cmut__lang__function__skip_unparsed __cmut__lang__function__init_param__ARG_SKIP_UNPARSED)
 
+    cmut__lang__set_in_parent_scope( __cmut__lang__function__option_list "" )
+    cmut__lang__set_in_parent_scope( __cmut__lang__function__param_list "" )
+    cmut__lang__set_in_parent_scope( __cmut__lang__function__multi_param_list "" )
+
+    cmut__log__debug_if( cmut__lang__function__init_param
+                         "start parameter(s) definition of \"${function_name}\"."
+                         CMUT__LANG__FUNCTION__DEBUG )
+
 endfunction()
 
 function( cmut__lang__function__add_option )
 
     list(APPEND __cmut__lang__function__option_list ${ARGN} )
     cmut__lang__forward_in_parent_scope( __cmut__lang__function__option_list )
+
+    cmut__log__debug_if( cmut__lang__function__add_option
+                         "add option(s) \"${ARGN}\"."
+                         CMUT__LANG__FUNCTION__DEBUG )
 
 endfunction()
 
@@ -25,6 +38,10 @@ function( cmut__lang__function__add_exclusive_option name )
 
     list(APPEND __cmut__lang__function__exclusive_option_list__${name} ${ARGN} )
     cmut__lang__forward_in_parent_scope( __cmut__lang__function__exclusive_option_list__${name} )
+
+    cmut__log__debug_if( cmut__lang__function__add_exclusive_option
+                         "add exclusive options \"${ARGN}\"."
+                         CMUT__LANG__FUNCTION__DEBUG )
 
 endfunction()
 
@@ -41,12 +58,20 @@ function( cmut__lang__function__add_param param_name )
     list(APPEND __cmut__lang__function__param_list ${param_name} )
     cmut__lang__forward_in_parent_scope( __cmut__lang__function__param_list )
 
+    cmut__log__debug_if( cmut__lang__function__add_param
+                         "add parameter \"${param_name}\", (default:${__cmut__lang__function__add_param__ARG_DEFAULT})."
+                         CMUT__LANG__FUNCTION__DEBUG )
+
 endfunction()
 
 function( cmut__lang__function__add_multi_param multi_param_name )
 
     list(APPEND __cmut__lang__function__multi_param_list ${multi_param_name} )
     cmut__lang__forward_in_parent_scope( __cmut__lang__function__multi_param_list )
+
+    cmut__log__debug_if( cmut__lang__function__add_multi_param
+                         "add multi-values parameter \"${multi_param_name}\"."
+                         CMUT__LANG__FUNCTION__DEBUG )
 
 endfunction()
 
@@ -59,18 +84,21 @@ macro( cmut__lang__function__parse_arguments )
         "${__cmut__lang__function__multi_param_list}"
         ${ARGN}
         )
-
-    if(CMUT__LANG__FUNCTION__PARSE_ARGUMENTS__DEBUG)
-        cmut_debug("[cmut][lang][function][parse_arguments]: in function \"__cmut__lang__function__function_name\": arguments parsed :")
+    if(CMUT__LANG__FUNCTION__DEBUG)
+        set( MESSAGE "\n-- In function \"${__cmut__lang__function__function_name}\": arguments parsed :")
         foreach(v ${__cmut__lang__function__option_list})
-            cmut_debug("-- ${__cmut__lang__function__arg_prefix}_${v} = ${${__cmut__lang__function__arg_prefix}_${v}}")
+            string( APPEND MESSAGE "\n--    ${v} = ${${__cmut__lang__function__arg_prefix}_${v}}")
         endforeach()
         foreach(v ${__cmut__lang__function__param_list})
-            cmut_debug("-- ${__cmut__lang__function__arg_prefix}_${v} = ${${__cmut__lang__function__arg_prefix}_${v}}")
+            string( APPEND MESSAGE "\n--    ${v} = ${${__cmut__lang__function__arg_prefix}_${v}}")
         endforeach()
         foreach(v ${__cmut__lang__function__multi_param_list})
-            cmut_debug("-- ${__cmut__lang__function__arg_prefix}_${v} = ${${__cmut__lang__function__arg_prefix}_${v}}")
+            string( APPEND MESSAGE "\n--    ${v} = ${${__cmut__lang__function__arg_prefix}_${v}}")
         endforeach()
+
+        cmut__log__debug_if( cmut__lang__function__parse_arguments
+                             "${MESSAGE}"
+                             CMUT__LANG__FUNCTION__DEBUG )
     endif()
 
     foreach(param IN LISTS __cmut__lang__function__param_list)
