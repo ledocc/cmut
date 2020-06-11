66d09f323e3433429d327a2182b411ddafbfaabf
David Callu <callu.david@gmail.com>
2020-06-11 12:20:38 +0200

    typo(config): fix english

--- config/cmut__config__default_build_type.cmake
+++ config/cmut__config__default_build_type.cmake
@@ -9,8 +9,9 @@ function(cmut__config__default_build_type default_build_type)
             return()
         endif()
 
-        cmut_info("[cmut][config][default_build_type]: Setting build type to '${default_build_type}' as none was specified.")
+        cmut_info("[cmut][config][default_build_type]: Setting build type to '${default_build_type}' as no one is specified.")
         set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE STRING "Choose the type of build." FORCE)
+
         # Set the possible values of build type for cmake-gui
         set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 
