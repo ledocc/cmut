7285f013fb3be2c2306b189c3e9a5cac2f4f0b24
David Callu <callu.david@gmail.com>
2020-11-18 21:42:26 +0100

    fix(library): add Xcode 11 support

--- library/macos/get_link_type.sh
+++ library/macos/get_link_type.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-${1} -private-header "${2}" | tail -n1  | tr -s " " | cut -d' ' -f 5 
+
+version=$(${1} --version | head -n1 | cut -d" " -f4|cut -d. -f1)
+
+
+if ((${version} <= 10))
+then
+    ${1} -private-header "${2}" | tail -n1  | tr -s " " | cut -d' ' -f 5 
+else
+    ${1} --private-headers "${2}" | sed -n 6p  | tr -s " " | cut -d' ' -f 5 
+fi
