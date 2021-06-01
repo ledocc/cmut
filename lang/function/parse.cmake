5e9b4aa59b6a1e37b5ac5f9eb63c92188a5e9718
David Callu <callu.david@gmail.com>
2021-06-01 08:48:29 +0200

    feat(lang): improve cmut__lang__function by automagically do initialisation

--- lang/function/parse.cmake
+++ lang/function/parse.cmake
@@ -1,25 +1,85 @@
 #set(CMUT__LANG__FUNCTION__DEBUG 1)
 
+
+macro( cmut__lang__function__get_current_function result )
+    if( DEFINED CMAKE_CURRENT_FUNCTION )
+        set( ${result} ${CMAKE_CURRENT_FUNCTION} )
+    elseif( DEFINED CMUT__LANG__FUNCTION__CURRENT_FUNCTION )
+        set( ${result} ${CMUT__LANG__FUNCTION__CURRENT_FUNCTION} )
+    else()
+        unset( ${result} )
+    endif()
+endmacro()
+
+
+macro( cmut__lang__function__set_initialized function_name )
+    cmut__lang__set_in_parent_scope( __cmut__lang__function__${function_name}__param_initialized 1 )
+endmacro()
+
+function( cmut__lang__function__is_initialized result function_name )
+    cmut__lang__return( __cmut__lang__function__${function_name}__param_initialized )
+endfunction()
+
+macro( cmut__lang__function__return_if_initialized function_name )
+    cmut__lang__function__is_initialized( is_initialized ${function_name} )
+    if( is_initialized )
+        return()
+    endif()
+endmacro()
+
+
+
+
+
+
 function( cmut__lang__function__init_param function_name )
+
+    cmut__lang__function__return_if_initialized( ${function_name} )
+    cmut__lang__set_in_parent_scope( CMUT__LANG__FUNCTION__CURRENT_FUNCTION ${function_name} )
+
+    cmut__log__debug_if( cmut__lang__function__init_param
+        "Initialize parameter(s) definition of \"${function_name}\"."
+        CMUT__LANG__FUNCTION__DEBUG )
+
 
     cmake_parse_arguments( __cmut__lang__function__init_param__ARG "SKIP_UNPARSED" "" "" ${ARGN} )
 
     cmut__lang__set_in_parent_scope( __cmut__lang__function__function_name ${function_name} )
     cmut__lang__set_in_parent_scope( __cmut__lang__function__arg_prefix ARG)
     cmut__lang__set_in_parent_scope( __cmut__lang__function__skip_unparsed __cmut__lang__function__init_param__ARG_SKIP_UNPARSED)
-
     cmut__lang__set_in_parent_scope( __cmut__lang__function__option_list "" )
     cmut__lang__set_in_parent_scope( __cmut__lang__function__exclusive_option_list "" )
     cmut__lang__set_in_parent_scope( __cmut__lang__function__param_list "" )
     cmut__lang__set_in_parent_scope( __cmut__lang__function__multi_param_list "" )
 
-    cmut__log__debug_if( cmut__lang__function__init_param
-                         "start parameter(s) definition of \"${function_name}\"."
-                         CMUT__LANG__FUNCTION__DEBUG )
+
+    cmut__lang__function__set_initialized( ${function_name} )
 
 endfunction()
 
-function( cmut__lang__function__add_option )
+macro( cmut__lang__function__auto_init_or_fatal_error )
+    cmut__lang__function__get_current_function( current_function_name )
+    if( DEFINED current_function_name )
+        cmut__lang__function__init_param( ${current_function_name} )
+    else()
+        cmut__log__fatal( cmut__lang__function__auto_init_or_fatal_error  "You need to define CMAKE_CURRENT_FUNCTION or CMUT__LANG__FUNCTION__CURRENT_FUNCTION to use cmut__lang__function API, or call cmut__lang__function__init_param( <function_name> ) to init cmut__lang__function API.")
+        return()
+    endif()
+endmacro()
+
+macro( cmut__lang__function__add_option )
+    cmut__lang__function__auto_init_or_fatal_error()
+    cmut__lang__function__add_option__impl( ${ARGV} )
+endmacro()
+
+macro( cmut__lang__function__add_param )
+    cmut__lang__function__auto_init_or_fatal_error()
+    cmut__lang__function__add_param__impl( ${ARGV} )
+endmacro()
+
+
+
+function( cmut__lang__function__add_option__impl )
 
     list(APPEND __cmut__lang__function__option_list ${ARGN} )
     cmut__lang__forward_in_parent_scope( __cmut__lang__function__option_list )
@@ -46,7 +106,7 @@ function( cmut__lang__function__add_exclusive_option name )
 
 endfunction()
 
-function( cmut__lang__function__add_param param_name )
+function( cmut__lang__function__add_param__impl param_name )
 
     cmake_parse_arguments( ARG "" "DEFAULT" "" ${ARGN} )
 
