6e7b1b365afec9c6e57f253ad7768662f3c3ca1d
David Callu <callu.david@gmail.com>
2021-06-01 08:46:26 +0200

    feat(project): add GIT_ROOT_DIR parameter to cmut__project__get_git_{revision_count,revision_hash}

--- project/cmut__project__get_git.cmake
+++ project/cmut__project__get_git.cmake
@@ -3,25 +3,31 @@
 function(cmut__project__get_git_revision_count result )
     set(CMAKE_CURRENT_FUNCTION cmut__project__get_git_revision_count)
 
+    cmut__lang__function__add_param( GIT_ROOT_DIR DEFAULT "${PROJECT_SOURCE_DIR}" )
+    cmut__lang__function__parse_arguments( ${ARGN} )
 
-    if ( NOT EXISTS "${PROJECT_SOURCE_DIR}/.git" )
+    if ( NOT EXISTS "${ARG_GIT_ROOT_DIR}/.git" )
         cmut__lang__return_unset()
         return()
     endif()
 
-    cmut__git__get_revision_count( revision_count GIT_DIR "${PROJECT_SOURCE_DIR}" )
+    cmut__git__get_revision_count( revision_count GIT_DIR "${ARG_GIT_ROOT_DIR}" )
     cmut__lang__return( revision_count )
 
 endfunction()
 
 function(cmut__project__get_git_revision_hash result)
+    set(CMAKE_CURRENT_FUNCTION cmut__project__get_git_revision_hash)
 
-    if ( NOT EXISTS "${PROJECT_SOURCE_DIR}/.git" )
+    cmut__lang__function__add_param( GIT_ROOT_DIR DEFAULT "${PROJECT_SOURCE_DIR}" )
+    cmut__lang__function__parse_arguments( ${ARGN} )
+
+    if ( NOT EXISTS "${ARG_GIT_ROOT_DIR}/.git" )
         cmut__lang__return_unset()
         return()
     endif()
 
-    cmut__git__get_revision_hash( revision_hash GIT_DIR "${PROJECT_SOURCE_DIR}" )
+    cmut__git__get_revision_hash( revision_hash GIT_DIR "${ARG_GIT_ROOT_DIR}" )
     cmut__lang__return( revision_hash )
 
 endfunction()
